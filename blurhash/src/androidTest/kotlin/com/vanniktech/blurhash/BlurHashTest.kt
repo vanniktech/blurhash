4f06f2966b73fe5fba8190d82fbf1c5f78b1ef28
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-10-23 16:44:00 +0200

    Android: Paparazzi test for encode + decode. (#19)

--- blurhash/src/androidTest/kotlin/com/vanniktech/blurhash/BlurHashTest.kt
+++ blurhash/src/androidTest/kotlin/com/vanniktech/blurhash/BlurHashTest.kt
@@ -1,31 +1,42 @@
 package com.vanniktech.blurhash
 
-import android.graphics.Color
+import android.graphics.BitmapFactory
 import android.widget.ImageView
+import android.widget.LinearLayout
 import app.cash.paparazzi.DeviceConfig.Companion.PIXEL_5
 import app.cash.paparazzi.Paparazzi
-import app.cash.paparazzi.androidHome
-import app.cash.paparazzi.detectEnvironment
-import org.junit.Ignore
 import org.junit.Rule
+import java.io.File
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class BlurHashTest {
   @get:Rule
   val paparazzi = Paparazzi(
     deviceConfig = PIXEL_5,
     theme = "android:Theme.Material.Light.NoActionBar",
-    environment = detectEnvironment().copy(
-      platformDir = "${androidHome()}/platforms/android-32",
-      compileSdkVersion = 32,
-    ),
   )
 
-  @Test @Ignore("Layout lib crash") fun launchView() {
-    val view = ImageView(paparazzi.context)
-    view.layoutParams.height = 50
-    view.layoutParams.width = 50
-    view.setBackgroundColor(Color.RED)
-    paparazzi.snapshot(view)
+  @Test fun encodeDecode() {
+    val blurHash = BlurHash.encode(BitmapFactory.decodeStream(workspaceDirectory().resolve("sample-android/src/main/res/drawable-nodpi/blueberries.jpg").inputStream()), 5, 4)
+    assertEquals(expected = "V4BhTMQyJ]iErP8wM^%ht?tboYkPtSiWXtMKSrxcSst7", actual = blurHash)
+
+    val imageView = ImageView(paparazzi.context)
+    val density = paparazzi.context.resources.displayMetrics.density
+    val width = (300 * density).toInt()
+    val height = (200 * density).toInt()
+    imageView.setImageBitmap(BlurHash.decode(blurHash, width, height))
+    paparazzi.snapshot(
+      LinearLayout(paparazzi.context).apply {
+        addView(imageView, LinearLayout.LayoutParams(width, height))
+      },
+    )
   }
+
+  private fun workspaceDirectory() = File(BlurHashTest::class.java.classLoader?.getResource(".")?.file!!)
+    .parentFile
+    ?.parentFile
+    ?.parentFile
+    ?.parentFile
+    ?.parentFile!!
 }
