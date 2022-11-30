56df00793cf39dd3ca791b09e0c6de9f169e04b5
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-11-30 12:15:49 +0100

    Gradle: Nuke Dependency Gurad plugin.

--- sample-android/build.gradle.kts
+++ sample-android/build.gradle.kts
@@ -1,11 +1,6 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
-  id("com.dropbox.dependency-guard")
-}
-
-dependencyGuard {
-  configuration("releaseRuntimeClasspath")
 }
 
 android {
